140047ff1449bf910f4a6eb6e7a467758fa58c8c
Arnaud SCOTE <arnaud.scote@netapsys.fr>
2015-11-04 11:44:09 +0100

    [ADD] getItems pour document recipientgrouplist

--- persistentdocument/recipientGroupList.class.php
+++ persistentdocument/recipientGroupList.class.php
@@ -1,4 +1,11 @@
 <?php
 class form_persistentdocument_recipientGroupList extends form_persistentdocument_recipientGroupListbase
 {
+	public function getItems()
+	{
+		$form = $this->getForm();
+		$formService = form_ListRecipientgrouplistService::getInstance();
+		$formService->setParentForm($form);
+		return $formService->getItems();
+	}
 }
